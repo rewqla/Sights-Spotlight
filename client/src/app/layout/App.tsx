177c4381f6d3187f9f7296c861cf0de2157e3ea1
rewqla <desares526@gmail.com>
2024-02-12 20:23:50 +0200

    Create initial folder structure for the client and the futher work

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>HIII</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>Super header</h1>
+    </header>
+  );
+};
+
+export default Header;
