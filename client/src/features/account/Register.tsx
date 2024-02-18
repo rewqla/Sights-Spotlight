38bc628c46be7e674ff6bd4e397e71331d1249c4
rewqla <desares526@gmail.com>
2024-02-18 09:47:30 +0200

    Add simple styles for the account

--- client/src/features/account/Register.tsx
+++ client/src/features/account/Register.tsx
@@ -1,7 +1,33 @@
+import "./Account.css";
 const Register = () => {
   return (
     <div>
-      <h1>Register for beginners</h1>
+      <h1>Register</h1>
+      <form className="form-container">
+        <div className="form-group">
+          <label className="form-label">First name:</label>
+          <input type="text" className="form-input" />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Last name:</label>
+          <input type="text" className="form-input" />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Username:</label>
+          <input type="text" className="form-input" />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Email:</label>
+          <input type="email" className="form-input" />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Password:</label>
+          <input type="password" className="form-input" />
+        </div>
+        <button type="submit" className="form-button">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
