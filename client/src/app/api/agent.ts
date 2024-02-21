480d0bea1d2742b58f8e805472a3da6896e50de3
rewqla <desares526@gmail.com>
2024-02-21 15:00:13 +0200

    Add axios for a futher requests

---
+++ client/src/app/api/agent.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://localhost:7188/";
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
+const Account = {
+  login: (values: any) =>
+    axios.post("account/login", values).then((response) => response.data),
+  register: (values: any) =>
+    axios.post("account/register", values).then((response) => response.data),
+};
+
+export default Account;
